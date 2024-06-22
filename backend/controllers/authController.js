fc699b8459b1f73a3076999c3c76fbaed3a9b938
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 <picard.boris@gmail.com>
2024-06-22 23:13:30 +0200

    feat: add validation schema with zod & add bloc in createuser

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,5 @@
 import User from "../models/userModel.js";
+import userSchema from "../schemas/userSchema.js";
 
 export const signUp = async (req, res) => {
   const { mail, password } = req.body;
@@ -6,6 +7,12 @@ export const signUp = async (req, res) => {
   if (!mail || !password) {
     res.status(401).json({ error: "All fields are required !" });
     return;
+  }
+
+  try {
+    userSchema.parse({ mail, password });
+  } catch (error) {
+    return res.status(400).json({ error: error.errors });
   }
 
   // const existingUser = await User.findOne({ mail });
@@ -16,7 +23,6 @@ export const signUp = async (req, res) => {
   // }
 
   try {
-    
     const user = await User.create({ mail, password });
 
     const token = await user.generateToken();
