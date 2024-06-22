e9b23dab4f427995b6a7c3e0219616a4d74a2032
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 <picard.boris@gmail.com>
2024-06-22 12:00:12 +0200

    feat(userModel): add transactions role & coins

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,16 +2,39 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  mail: {
-    type: String,
-    required: true,
+const userSchema = new Schema(
+  {
+    mail: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    transactions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Transaction",
+      },
+    ],
+    coins: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Coin",
+      },
+    ],
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const user = mongoose.model("User", userSchema);
 
