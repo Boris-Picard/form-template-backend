a36a3f386c17ec52eb032c67e71aec49bd5e3380
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 <picard.boris@gmail.com>
2024-06-23 17:34:29 +0200

    feat: first test for validation ok

--- backend/tests/authController.test.js
+++ backend/tests/authController.test.js
@@ -1,4 +1,30 @@
 import { signUp } from "../controllers/authController.js";
-import User from "../models/userModel";
 import { jest } from "@jest/globals";
 
+describe("User Controller - signUp", () => {
+  let req, res;
+
+  beforeEach(() => {
+    req = { body: { mail: "", password: "" } };
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+  });
+
+  it("should return validation errors if email or password is invalid", async () => {
+    req.body.mail = "invalid-email";
+    req.body.password = "short";
+
+    await signUp(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        error: expect.any(Array),
+      })
+    );
+  });
+
+
+});
