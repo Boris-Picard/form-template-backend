2a1bc1bd3e6f15a579c30b409992948f05122353
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 <picard.boris@gmail.com>
2024-06-24 12:31:15 +0200

    refactor: change req.body for first test & add comments

--- backend/tests/authController.test.js
+++ backend/tests/authController.test.js
@@ -19,17 +19,21 @@ describe("User Controller - signUp", () => {
     };
   });
 
-  // test if validation schema is valid
+  // Teste si le schéma de validation est valide
   it("should return validation errors if email or password is invalid", async () => {
-    req.body.mail = "invalid-email";
-    req.body.password = "short";
+    // Prépare les données de requête avec un email invalide et un mot de passe trop court
+    req.body = { mail: "invalid-email", password: "short" };
 
+    // Mocke userSchema.parse pour générer une exception avec des erreurs spécifiques
+    // mockImplementationOnce au lieu de mockResolvedValue pour les exceptions
     userSchema.parse.mockImplementationOnce(() => {
       throw { errors: ["Email is invalid", "Password is too short"] };
     });
 
+    // Appelle la fonction signUp avec les données de requête
     await signUp(req, res);
 
+    // Vérifie que la réponse a un statut 400 et retourne un objet JSON contenant des erreurs
     expect(res.status).toHaveBeenCalledWith(400);
     expect(res.json).toHaveBeenCalledWith(
       expect.objectContaining({
@@ -38,15 +42,21 @@ describe("User Controller - signUp", () => {
     );
   });
 
-  // test if user already exist
+  // Teste si un utilisateur existe déjà
   it("should return error if mail already exist", async () => {
+    // Prépare les données de requête avec un email existant et un mot de passe valide
     req.body = { mail: "test@test.com", password: "validPassword123" };
 
+    // Mocke User.findOne pour simuler la présence d'un utilisateur avec le même email
     User.findOne.mockResolvedValue({ mail: req.body.mail });
 
+    // Appelle la fonction signUp avec les données de requête
     await signUp(req, res);
 
+    // Vérifie que User.findOne a été appelé avec l'email de la requête
     expect(User.findOne).toHaveBeenCalledWith({ mail: req.body.mail });
+
+    // Vérifie que la réponse a un statut 401 et retourne un objet JSON avec une erreur d'utilisateur existant
     expect(res.status).toHaveBeenCalledWith(401);
     expect(res.json).toHaveBeenCalledWith({ error: "User already exist" });
   });
