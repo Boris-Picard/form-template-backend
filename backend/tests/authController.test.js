7782cd780ccb311f8cf0048f6687e0c7e0425f20
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 <picard.boris@gmail.com>
2024-06-24 12:13:45 +0200

    feat: parsing test done

--- backend/tests/authController.test.js
+++ backend/tests/authController.test.js
@@ -1,10 +1,17 @@
 import { signUp } from "../controllers/authController.js";
 import { jest } from "@jest/globals";
+import User from "../models/userModel.js";
+import userSchema from "../schemas/userSchema.js";
+
+jest.mock("../models/userModel.js");
+jest.mock("../schemas/userSchema.js");
 
 describe("User Controller - signUp", () => {
   let req, res;
 
   beforeEach(() => {
+    jest.clearAllMocks();
+    User.findOne = jest.fn();
     req = { body: { mail: "", password: "" } };
     res = {
       status: jest.fn().mockReturnThis(),
@@ -12,9 +19,14 @@ describe("User Controller - signUp", () => {
     };
   });
 
+  // test if validation schema is valid
   it("should return validation errors if email or password is invalid", async () => {
     req.body.mail = "invalid-email";
     req.body.password = "short";
+
+    userSchema.parse.mockImplementation(() => {
+      throw { errors: ["Email is invalid", "Password is too short"] };
+    });
 
     await signUp(req, res);
 
@@ -26,5 +38,16 @@ describe("User Controller - signUp", () => {
     );
   });
 
+  // test if user already exist
+  it("should return error if mail already exist", async () => {
+    req.body = { mail: "test@test.com", password: "validPassword123" };
 
+    User.findOne.mockResolvedValue({ mail: req.body.mail });
+
+    await signUp(req, res);
+
+    expect(User.findOne).toHaveBeenCalledWith({ mail: req.body.mail });
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith({ error: "User already exist" });
+  });
 });
