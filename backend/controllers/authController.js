42532eaa190786eedeedb5b106ef1b9dd128dda4
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 <picard.boris@gmail.com>
2024-06-25 12:37:37 +0200

    fix: res status 200 if user found

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -92,6 +92,8 @@ export const getUser = async (req, res) => {
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
+
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
   }
