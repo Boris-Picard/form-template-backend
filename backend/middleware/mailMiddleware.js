2c4e2df7a175a74a964c4ce942ae46e49c3dc2f8
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 <picard.boris@gmail.com>
2024-07-03 16:18:17 +0200

    feat(mailMiddleware): modify design to send email

--- backend/middleware/mailMiddleware.js
+++ backend/middleware/mailMiddleware.js
@@ -1,8 +1,8 @@
 import nodemailer from "nodemailer";
 import process from "process";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config(); 
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.hostinger.com",
@@ -18,8 +18,18 @@ const sendVerificationEmail = (userEmail, token) => {
   const mailOptions = {
     from: process.env.MAIL_USER,
     to: userEmail,
-    subject: "Email Verification",
-    html: `<p>Click <a href="http://localhost:5173/verify-email?token=${token}">here</a> to verify your email.</p>`,
+    subject: "Email Verification to myCryptoFolio",
+    html: `
+      <div style="font-family: Arial, sans-serif; max-width: 600px; margin: auto; padding: 20px; border: 1px solid #dddddd; border-radius: 10px;">
+        <h2 style="color: #333333;">Verify Your Email Address</h2>
+        <p style="color: #555555;">Thank you for registering with us! Please click the button below to verify your email address and complete your registration:</p>
+        <div style="text-align: center; margin: 20px 0;">
+          <a href="http://localhost:5173/verify-email?token=${token}" style="background-color: #007BFF; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px;">Verify Email</a>
+        </div>
+        <p style="color: #555555;">If you did not create an account, no further action is required.</p>
+        <p style="color: #555555;">Best regards,<br>myCryptoFolio</p>
+      </div>
+    `,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
