04e649d89e911e03468c56f6191bf5497bd3a53e
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 <picard.boris@gmail.com>
2024-07-05 18:13:28 +0200

    feat: definitive max limiter

--- backend/middleware/rateLimitMiddleware.js
+++ backend/middleware/rateLimitMiddleware.js
@@ -2,12 +2,12 @@ import rateLimit from "express-rate-limit";
 
 export const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 50,
+  max: 5,
 });
 
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 50,
+  max: 100,
 });
 
 export const mailLimiter = rateLimit({
