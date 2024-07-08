493376d11edf174a036f7ef5deaada24163efea3
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 <picard.boris@gmail.com>
2024-07-08 17:29:25 +0200

    fix: cookie to samesite fixed as lax

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -141,7 +141,7 @@ export const signIn = async (req, res) => {
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "Strict",
+      sameSite: "Lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
@@ -149,7 +149,7 @@ export const signIn = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "Strict",
+      sameSite: "Lax",
       maxAge: 1 * 60 * 60 * 1000, // 1 hour
     });
 
@@ -285,7 +285,7 @@ export const refreshToken = async (req, res) => {
     res.cookie("token", newToken, {
       httpOnly: true,
       secure: true,
-      sameSite: "Strict",
+      sameSite: "Lax",
       maxAge: 1 * 60 * 60 * 1000, // 1 hour
     });
 
