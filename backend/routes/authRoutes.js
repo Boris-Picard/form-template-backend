edc66a292388022747e041da1f4e51c19b9b8d3e
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 <picard.boris@gmail.com>
2024-07-08 16:57:33 +0200

    feat: deleted function in authroutes delete users

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -38,17 +38,4 @@ router.get("/verify-email", authMail, mailLimiter, verifyEmail);
 
 router.delete("/logout", logout);
 
-// seulement valable pour les tests et vider la collection
-router.delete("/deleteAllUsers", async (req, res) => {
-  try {
-    await User.deleteMany({});
-    res.status(200).send("Toutes les données ont été supprimées.");
-  } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .send("Une erreur est survenue lors de la suppression des données.");
-  }
-});
-
 export default router;
