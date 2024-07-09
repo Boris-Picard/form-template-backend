df01942471bc3b66f9d5e5358d550689bbe0d349
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 <picard.boris@gmail.com>
2024-07-09 11:38:13 +0200

    fix: fixed static file

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,13 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import process from "process";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url"; // Importer pour définir __dirname
 import coinRoutes from "./routes/coinRoutes.js";
 import transactionsRoutes from "./routes/transactionsRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import cookieParser from "cookie-parser";
 import { authLimiter, apiLimiter } from "./middleware/rateLimitMiddleware.js";
+import { fileURLToPath } from "url";
+import path from "path";
 
 // Charger les variables d'environnement à partir du fichier .env
 dotenv.config();
@@ -38,24 +38,18 @@ app.use(cookieParser());
 // Middleware pour parser les requêtes JSON
 app.use(express.json());
 
-// Définir l'en-tête Referrer-Policy
-app.use((req, res, next) => {
-  res.header("Referrer-Policy", "strict-origin-when-cross-origin");
-  next();
-});
-
 // Vos routes API
 app.use("/api/coin/", apiLimiter, coinRoutes);
 app.use("/api/transaction/", apiLimiter, transactionsRoutes);
 app.use("/api/auth/", authLimiter, authRoutes);
 
-// Servir les fichiers statiques de l'application React
-app.use(express.static(path.join(__dirname, "dist")));
+// Use the client app
+app.use(express.static(path.join(__dirname, "client/dist")));
 
-// Catch-all handler pour envoyer index.html pour toute requête non API
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
+// Render client for any path
+app.use("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "client/dist/index.html"))
+);
 
 // Gestion des erreurs pour les routes non trouvées
 app.use((req, res, next) => {
